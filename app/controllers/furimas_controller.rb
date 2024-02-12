c5c655a098dd34a82a4a977ffe8a925b47a322c9
Rui-Tanizaki <155947121+Rui-Tanizaki@users.noreply.github.com>
2024-02-12 16:28:23 +0900

    itemsテーブル作成

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,6 +1,17 @@
 class FurimasController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id if current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
